99cc7216e5bdbf7bb1f75a8410fd9dff1b7e4d83
Seba9292 <sebaszym92@gmail.com>
2022-07-23 14:48:25 +0200

    Wjazd do klubu

--- src/Zadanie.java
+++ src/Zadanie.java
@@ -1,25 +1,32 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Zadanie {
     public static void main(String[] args) {
-        /**
-         * 1. Chce aby w programie bylo pytanie o imie i nazwisko i wiek
-         * 2. chce zeby po zapytaniu o wiek byla petla sprawdzajaca czy osoba jest pelnoletnia (przy nie napis wypad)
-         */
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("podaj imie");
-        String imie = scanner.next();
-        System.out.println("podaj nazwisko");
-        String nazwisko = scanner.next();
-        System.out.println("podaj wiek");
-        int wiek = scanner.nextInt();
-        if (wiek >= 18){
-            System.out.println("zapraszam do klubu");
-        }
-        if (wiek < 18){
-            System.out.println("wypad");
-        }
+            /**
+             * 1.chce aby w programie było pytanie o imie i nazwisko i wiek
+             * 2.chce zeby po zapytaniu o wiek byla petla sprawdzajaca czy osoba jest pelnoletnia czy nie - (przy pelnoletniej napis zapraszam do klubu, a jezeli nie to napis wypad)
+             *
+             */
+            String[] wygrani = new String[]{"Monia", "Seba", "Magda", "Piotrek", "Adam"};
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("podaj imie");
+            String imie = scanner.next();
+            System.out.println("podaj nazwisko");
+            String naziwsko = scanner.next();
+            System.out.println("Podaj wiek");
+            int wiek = scanner.nextInt();
+            if (wiek>=18){
+                System.out.println("zapraszam do klubu");
+                for (String wygrany: wygrani){
+                    if(Objects.equals(imie, wygrany)){
+                        System.out.println("Dostajesz darmowego szota ;)");
+                    }
+                }
+            }
+            if(wiek<18){
+                System.out.println("wypad xD");
+            }
 
-
         }
-        }
+    }
