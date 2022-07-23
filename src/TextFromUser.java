cf962eeb2ebcc7c595c7fc1fa63dc2a7287fb75f
Seba9292 <sebaszym92@gmail.com>
2022-07-23 12:13:15 +0200

    Dodalem Scanner

--- src/TextFromUser.java
+++ src/TextFromUser.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 
 public class TextFromUser {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        System.out.println("Hello" + userInput);
+        System.out.println("Podaj Imie ");
+        String Seba = scanner.nextLine();
+        System.out.println("Podaj Nazwisko ");
+        String Szymanski = scanner.nextLine();
+        System.out.println("Podaj Wiek");
+        int wiek = scanner.nextInt();
+        System.out.println("Witaj " + "Imie " + "Nazwisko " + "Masz " +wiek + " lat " );
+
     }
 }
